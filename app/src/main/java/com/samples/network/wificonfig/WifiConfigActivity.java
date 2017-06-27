b1a563a516495ef9d06008ca98f0bff06b7da768
java2019 <java_2019@mail.ru>
2017-06-27 15:18:31 +0300

    test1

--- app/src/main/java/com/samples/network/wificonfig/WifiConfigActivity.java
+++ app/src/main/java/com/samples/network/wificonfig/WifiConfigActivity.java
@@ -1,13 +1,19 @@
 package com.samples.network.wificonfig;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
 
-public class WifiConfigActivity extends AppCompatActivity {
+public class WifiConfigActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_config);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
     }
 }
